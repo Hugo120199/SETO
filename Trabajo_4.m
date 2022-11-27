2088db4b91ab7791ba96a02895d2c98f34b3f1bb
raulmorenog <raul7santoto@hotmail.com>
2022-11-27 20:10:08 +0100

    cambios

--- Trabajo_4.m
+++ Trabajo_4.m
@@ -75,14 +75,14 @@ figure(1)
 subplot(1,2,1)
 plot(r,Ca,'b-')
 grid on;
-xlabel('$r [m]$','interpreter','latex','fontsize',12)
-ylabel('$C_a (r)$ $[Ns/m^{2}]$','interpreter','latex','fontsize',12)
+xlabel('$r$ [m]','interpreter','latex','fontsize',14)
+ylabel('$C_a$ $(r)$ [Ns/m$^{2}$]','interpreter','latex','fontsize',14)
 
 subplot(1,2,2)
 plot(r,fb_zh1,'r-')
 grid on;
-xlabel('$r [m]$','interpreter','latex','fontsize',12)
-ylabel('$F_{b,zH1} (r) [N/m]$','interpreter','latex','fontsize',12)
+xlabel('$r$ [m]','interpreter','latex','fontsize',14)
+ylabel('$F_{b,zH1}$ $(r)$ [N/m]','interpreter','latex','fontsize',14)
 
 %% Análisis modal
     % Características estructurales de la pala
@@ -97,8 +97,8 @@ f1 = omega1/(2*pi);
 figure(2)
 plot(r,w)
 grid on;
-xlabel('$r$ [m]','interpreter','latex','fontsize',12)
-ylabel('$w (r)$ [m]','interpreter','latex','fontsize',12)
+xlabel('$r$ [m]','interpreter','latex','fontsize',14)
+ylabel('$\psi_1$','interpreter','latex','fontsize',14)
 
     % Cálculo de las características modales
 m1 = trapz(r,psi1.^2.*m);
@@ -118,54 +118,54 @@ disp(string(omega1/(2*pi)) + " | " + string(m1) + " | " + string(c1) + " | " + s
 H1 = tf(1,m1*[1, 2*chi_1*omega1, omega1^2]);
 [mag,phase,wout] = bode(H1);
 
-figure()
+figure(3)
 subplot(2,1,1)
 semilogx(wout/(2*pi),20*log10(squeeze(mag(1,1,:))),"-","Color",'black');
-ylabel("$Magnitud [dB]$",'Interpreter','latex','fontsize',12);
-xlabel("$f [Hz]$",'Interpreter','latex','fontsize',12);
+ylabel("$Magnitud$ [dB]",'Interpreter','latex','fontsize',14);
+xlabel("$f$ [Hz]",'Interpreter','latex','fontsize',14);
 xlim([wout(1)/(2*pi) wout(end)/(2*pi)]);
 grid minor;
 
 subplot(2,1,2)
 semilogx(wout/(2*pi),squeeze(phase(1,1,:)),"-","Color",'black');
-ylabel("$Fase [^{\circ}]$",'Interpreter','latex','fontsize',12);
-xlabel("$f [Hz]$",'Interpreter','latex','fontsize',12);
+ylabel("$Fase$ $[^{\circ}]$",'Interpreter','latex','fontsize',14);
+xlabel("$f$ [Hz]",'Interpreter','latex','fontsize',14);
 xlim([wout(1)/(2*pi) wout(end)/(2*pi)]);
 grid minor;
 
     % Comparación de f1 con los distintos modelos (esta en el libro esto también)
- f1_fast = 1.0793;  %f1_fast = 0.6664; ??? % Esto es por estar en bandera
- error_fast = abs(f1_fast - f1)/f1;
- f1_adams = 1.074;  %f1_adams = 0.6292; ???
- error_adams = abs(f1_adams - f1)/f1;
+f1_fast = 1.0793;  %f1_fast = 0.6664; ??? % Esto es por estar en bandera
+error_fast = abs(f1_fast - f1)/f1;
+f1_adams = 1.074;  %f1_adams = 0.6292; ???
+error_adams = abs(f1_adams - f1)/f1;
 
- disp("---------------------------------------");
- disp("           |   f1   | f1_fast | f1_adams ");
- disp("Valor [Hz] | " + string(f1) + " | " + string(f1_fast) + " | " + string(f1_adams));
- disp("Error [%]  | " + " ----- | " + string(error_fast*100) + " | " + string(error_adams*100));
+disp("---------------------------------------");
+disp("           |   f1   | f1_fast | f1_adams ");
+disp("Valor [Hz] | " + string(f1) + " | " + string(f1_fast) + " | " + string(f1_adams));
+disp("Error [%]  | " + " ----- | " + string(error_fast*100) + " | " + string(error_adams*100));
 
 %% Distribución promedio de desplazamiento y momento flector de batimiento
     % Distribuación promedio de W 
 Fb_zh1_prom = 1./(m1*(omega1)^2).*trapz(r,fb_zh1'.*psi1);
 W_prom = psi1.*Fb_zh1_prom;
-    % Pequeños--> emportrada (pequeños desplazamietnos pero mayores momentos)I 
+    % Pequeños--> empotrada (pequeños desplazamientos pero mayores momentos)I 
     % y fuerzas asociadas a la posición de bandera (no gira y le entra
     % aire alineado a los perfiles---> resistencia pequeña)
-figure()
-plot(r,W_prom,'b-')
+figure(4)
+plot(r,W_prom,'k-')
 grid on;
-xlabel('$r [m]$','interpreter','latex','fontsize',12)
-ylabel('$W (r)$ [m]','interpreter','latex','fontsize',12)
+xlabel('$r [m]$','interpreter','latex','fontsize',14)
+ylabel('$W (r)$ [m]','interpreter','latex','fontsize',14)
 
     % Distribución promedio de momento flector (esto está con pinzas)
 for i = 1:length(r)
     Mb_yh1_prom(i) = 1./(m1*(omega1)^2).*trapz(r,fb_zh1'.*psi1.*r(i));
 end
-figure()
-plot(r,Mb_yh1_prom,'b-')
+figure(5)
+plot(r,Mb_yh1_prom,'k-')
 grid on;
-xlabel('$r [m]$','interpreter','latex','fontsize',12)
-ylabel('$Mb_{yH1} (r)$ [Nm]','interpreter','latex','fontsize',12)
+xlabel('$r [m]$','interpreter','latex','fontsize',14)
+ylabel('$Mb_{yH1} (r)$ [Nm]','interpreter','latex','fontsize',14)
 
 %% Varianza de desplazamiento y de momento flector de batimiento
 
